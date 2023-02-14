409e125a8a20fc349bb26712749afee7efd54df3
someshwarjha1 <someshwarjha7@gmail.com>
2023-02-15 00:56:23 +0530

    Frontend,crud added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show,:edit,:update,:destroy]
+    def show
+    end
+    def index
+        @articles = Article.all
+    end
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(set_params)
+        if @article.save
+            flash[:notice] = "Article saved successfully."
+            redirect_to @article
+        else 
+            render 'new'
+        end
+
+    end
+
+    def edit
+        
+    end
+
+    def update
+
+        
+        if @article.update(set_params)
+            flash[:notice] = "Edited Article successfully."
+            redirect_to @article
+        else 
+            render 'edit'
+        end
+    end
+
+    def destroy
+        if @article.destroy
+            redirect_to articles_path
+        else
+            flash[:alert] = "Not able to" 
+            render 'index'
+        end
+    end
+
+    private
+
+    def set_params
+        params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+end
